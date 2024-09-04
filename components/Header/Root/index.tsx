debab8480e58f3fe1b23f84f3b37d2e6abe853b0
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-09-04 11:19:09 -0300

    feat: :sparkles: Add "/" link on click at logo on the header component

--- components/Header/Root/index.tsx
+++ components/Header/Root/index.tsx
@@ -1,10 +1,15 @@
 import { ActionRow } from "../ActionsRow";
+import Link from "next/link";
 
 export const Root = () => {
   return (
     <header className="flex flex-row w-full px-4 h-12 border-b items-center justify-between sticky top-0 left-0 bg-transparent backdrop-blur-md">
       <div className="flex-1">
-        <h1 className="select-none tracking-tighter font-medium ">Saturn</h1>
+        <Link href="/">
+        <h1 className="select-none tracking-tighter font-medium hover:underline">
+          Saturn
+        </h1>
+        </Link>
       </div>
       <div className="">
         <ActionRow />
