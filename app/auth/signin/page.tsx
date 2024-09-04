6b05c4f3d3e30eba9427f61684d1686e8bc45fbc
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-09-04 11:21:51 -0300

    feat(New Page/Route): :sparkles: Created new auth sign in and sign up page

---
+++ app/auth/signin/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const SignInPage: NextPage = () => {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+};
+
+export default SignInPage;
---
+++ app/auth/signup/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const SignUpPage: NextPage = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+export default SignUpPage;
