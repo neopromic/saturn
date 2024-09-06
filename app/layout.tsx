3e293f8f0714b8c0614850fc0656236c9f7e3e8b
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-09-05 22:58:13 -0300

    feat: :mag: Improve SEO, add keywords, description, etc

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,38 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Saturn Saas",
+  title: "Saturn",
+  authors: [
+    { name: "Wesley Souza Laurentino" },
+    { name: "Creattek" },
+    {
+      name: "Neo",
+    },
+    { name: "Neopromic" },
+  ],
+  creator: "Wesley Souza Laurentino, Creattek",
+  keywords: [
+    "Tech",
+    "Next",
+    "next",
+    "wix",
+    "landing page",
+    "website builder",
+    "website",
+    "criar landing page",
+    "marketing digital",
+    "página de lançamento",
+    "funil de vendas",
+    "shopify",
+    "nuvemshop",
+    "ads",
+    "converter",
+    "como converter",
+    "como vender mais",
+    "primeira venda",
+  ],
+  description:
+    "Dê vida às suas ideias e aumente suas conversões com facilidade. Nossa ferramenta permite que você construa páginas poderosas e envolventes com um simples arrastar e soltar.",
 };
 
 export default function RootLayout({
