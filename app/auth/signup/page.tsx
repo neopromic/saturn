4a5795aa05d473c4ffa68d705ee9a8ac9b2025cf
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-09-14 15:01:16 -0300

    feat: :lipstick: working on the ui of sign up page

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,11 +1,36 @@
-import { NextPage } from "next";
+import BlurIn from "@/components/magicui/blur-in";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
-const SignUpPage: NextPage = () => {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
-  );
+const SignUpPage = () => {
+	return (
+		<div className="overflow-y-hidden flex flex-col items-center px-4 py-12">
+			<section className="flex flex-col ">
+				<h1 className="text-4xl font-bold">Comece a usar o saturn</h1>
+				<h2 className="text-muted-foreground text-sm">
+					Crie sua conta em nossa plataforma e comece a testar gratuítamente!
+				</h2>
+			</section>
+			<form className="space-y-4 mt-4 h-fit">
+				<div className="">
+					<Label htmlFor="username">Nome de usuário</Label>
+					<Input id="username" />
+				</div>
+				<div className="">
+					<Label htmlFor="email">Email</Label>
+				<Input id="email" />
+				</div>
+				<div className="">
+					<Label htmlFor="pass">Senha</Label>
+					<Input id="pass" />
+				</div>
+				<div className="">
+					<Label htmlFor="repeatpass">Repita sua senha</Label>
+					<Input id="repeatpass" />
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default SignUpPage;
