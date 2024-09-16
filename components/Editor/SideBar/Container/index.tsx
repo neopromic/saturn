3a1e4e10d13ef58de7631ec45f1ef9a1a2a17063
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-09-15 23:19:24 -0300

    feat: add some items on container

--- components/Editor/SideBar/Container/index.tsx
+++ components/Editor/SideBar/Container/index.tsx
@@ -1,7 +1,15 @@
+import { Sparkles } from "lucide-react";
+import { SideBarItems } from "../Items";
+
 export const SideBarContainer = () => {
 	return (
-		<aside className="w-full max-w-[20rem] h-[calc(100vh-2rem)] dark:bg-slate-900">
-			<div className="" />
+		<aside className="w-full max-w-[20rem] h-[calc(100vh-3rem)] dark:bg-slate-900 border-r-2 py-4">
+			<div className="px-4">
+				<SideBarItems>
+					<Sparkles size={16} />
+					Templates
+				</SideBarItems>
+			</div>
 		</aside>
 	);
 };
