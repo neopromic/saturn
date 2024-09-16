75ccce793e58c547bc501be382b659969230e585
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-09-16 11:09:18 -0300

    feat: add some more space in y

--- components/Header/Root/index.tsx
+++ components/Header/Root/index.tsx
@@ -2,18 +2,18 @@ import { ActionRow } from "../ActionsRow";
 import Link from "next/link";
 
 export const Root = () => {
-  return (
-    <header className="flex flex-row w-full px-4 h-12 border-b items-center justify-between sticky top-0 left-0 bg-transparent backdrop-blur-md">
-      <div className="flex-1">
-        <Link href="/">
-        <h1 className="select-none tracking-tighter font-medium hover:underline">
-          Saturn
-        </h1>
-        </Link>
-      </div>
-      <div className="">
-        <ActionRow />
-      </div>
-    </header>
-  );
+	return (
+		<header className="sticky left-0 top-0 flex h-fit max-h-16 w-full flex-row items-center justify-between border-b bg-transparent px-4 py-2 backdrop-blur-md">
+			<div className="flex-1">
+				<Link href="/">
+					<h1 className="select-none font-medium tracking-tighter hover:underline">
+						Saturn
+					</h1>
+				</Link>
+			</div>
+			<div className="">
+				<ActionRow />
+			</div>
+		</header>
+	);
 };
