e87d5512fe22a82ce0990ac7645eb159a85178d4
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-09-15 23:52:26 -0300

    feat: add some items on sidebar and add the verticalization

--- components/Editor/SideBar/Container/index.tsx
+++ components/Editor/SideBar/Container/index.tsx
@@ -4,10 +4,18 @@ import { SideBarItems } from "../Items";
 export const SideBarContainer = () => {
 	return (
 		<aside className="w-full max-w-[20rem] h-[calc(100vh-3rem)] dark:bg-slate-900 border-r-2 py-4">
-			<div className="px-4">
+			<div className="px-4 flex flex-col space-y-8">
 				<SideBarItems>
 					<Sparkles size={16} />
 					Templates
+				</SideBarItems>
+				<SideBarItems>
+					<Sparkles size={16} />
+					Estilos
+				</SideBarItems>
+				<SideBarItems>
+					<Sparkles size={16} />
+					Blocos
 				</SideBarItems>
 			</div>
 		</aside>
