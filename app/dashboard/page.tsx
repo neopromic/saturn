6b14d4a42c19cfb9573c05d4d0c21e93557980ec
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-09-16 11:40:07 -0300

    feat: add badge to warn about the editor

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,22 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-import { Sparkles } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { Sparkles, SparklesIcon } from "lucide-react";
 
 const Dashboard = () => {
 	return (
 		<main className="">
-			<div className="flex h-72 flex-col items-center justify-center space-y-2 bg-gradient-to-b from-blue-charcoal-500 to-background px-4 py-12 text-primary">
+			<div className="flex h-72 flex-col items-center justify-center space-y-2 bg-gradient-to-b from-blue-charcoal-500 via-blue-charcoal-400 to-background px-4 py-12 text-primary dark:from-blue-charcoal-500 dark:to-background">
+				<Badge className="gap-1">
+					<SparklesIcon size={14} />
+					<Separator orientation="vertical" />
+					Nosso editor está em desenvolvimento
+				</Badge>
 				<h1 className="max-w-[600px] text-center text-4xl font-bold leading-none tracking-tighter text-blue-charcoal-50 lg:text-6xl">
 					Gerencie suas landing pages num só lugar...
 				</h1>
-				<p className="text-center text-sm text-blue-charcoal-100/80">
+				<p className="text-center text-sm text-muted-foreground dark:text-blue-charcoal-100/80">
 					Gerencie todas as suas landing pages em um único lugar.
 				</p>
 
