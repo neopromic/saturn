8e662a69c3730cf318ef4993698de9d1ec73bd12
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-09-15 22:56:55 -0300

    feat: :sparkles: add a new component to atomize the editor layout

---
+++ components/Editor/SideBar/Container/index.tsx
@@ -0,0 +1,7 @@
+export const SideBarContainer = () => {
+	return (
+		<aside className="w-full max-w-[20rem] h-[calc(100vh-2rem)] dark:bg-slate-900">
+			<div className="" />
+		</aside>
+	);
+};
---
+++ components/Editor/SideBar/index.tsx
@@ -0,0 +1,5 @@
+import { SideBarContainer } from "./Container";
+
+export const SideBar = {
+	Container: SideBarContainer,
+};
