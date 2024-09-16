cb7dd9b0cb3abe25ade24d1fad9e6758d63811f5
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-09-16 10:51:42 -0300

    feat: typo changes at the header

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,37 +5,37 @@ import { Sparkles } from "lucide-react";
 const Dashboard = () => {
 	return (
 		<main className="">
-			<div className="px-4 py-12 flex flex-col space-y-2 bg-gradient-to-b from-blue-charcoal-500 to-background text-primary h-72 items-center justify-center">
-				<h1 className="text-4xl lg:text-6xl font-bold tracking-tighter leading-none max-w-[600px] text-blue-charcoal-50">
-					Seu dashboard de landing pages
+			<div className="flex h-72 flex-col items-center justify-center space-y-2 bg-gradient-to-b from-blue-charcoal-500 to-background px-4 py-12 text-primary">
+				<h1 className="max-w-[600px] text-center text-4xl font-bold leading-none tracking-tighter text-blue-charcoal-50 lg:text-6xl">
+					Gerencie suas landing pages num só lugar...
 				</h1>
-				<p className="text-blue-charcoal-100/80 text-sm">
+				<p className="text-center text-sm text-blue-charcoal-100/80">
 					Gerencie todas as suas landing pages em um único lugar.
 				</p>
 
-				<Button className="gap-2 w-fit mt-6" variant={"default"}>
+				<Button className="w-64 gap-2" variant={"blue"}>
 					<Sparkles size={18} />
 					Criar nova!
 				</Button>
 			</div>
 
 			<section className="px-4">
-				<h1 className="font-semibold text-lg">Modelos populares</h1>
+				<h1 className="text-lg font-semibold">Modelos populares</h1>
 				<p className="text-xs text-muted-foreground">
 					Veja os modelos mais utilizados pelos nossos usuários.
 				</p>
-				<div className="w-full border-2 my-4 border-dotted p-4 rounded-md">
+				<div className="my-4 w-full rounded-md border-2 border-dotted p-4">
 					<Card className="w-fit">
 						<CardContent className="size-32" />
 					</Card>
 				</div>
 			</section>
 			<section className="px-4">
-				<h1 className="font-semibold text-lg">Seus modelos</h1>
+				<h1 className="text-lg font-semibold">Seus modelos</h1>
 				<p className="text-xs text-muted-foreground">
 					Veja os modelos criados e mais utilizados por você.
 				</p>
-				<div className="w-full border-2 my-4 border-dotted p-4 rounded-md">
+				<div className="my-4 w-full rounded-md border-2 border-dotted p-4">
 					<Card className="w-fit">
 						<CardContent className="size-32" />
 					</Card>
