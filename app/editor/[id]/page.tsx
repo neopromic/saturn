639796a7a750e5c7cbefeacaaaab18d2df4e9d60
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-09-16 10:52:36 -0300

    feat: add the sidebar component and preview layout component

--- app/editor/[id]/page.tsx
+++ app/editor/[id]/page.tsx
@@ -1,12 +1,14 @@
+import { PreviewLayout } from "@/components/Editor/Preview";
 import { SideBar } from "@/components/Editor/SideBar";
 
 const EditorPage = () => {
 	return (
-		<div className="grid grid-cols-2 overflow-y-hidden">
+		<div className="flex overflow-y-hidden h-[calc(100vh-3rem)]">
 			<SideBar.Container />
-			<main className="w-full">
-				<h1>Hello, world</h1>
-			</main>
+
+			<PreviewLayout.Container>
+				<h1>Hello, preview!</h1>
+			</PreviewLayout.Container>
 		</div>
 	);
 };
