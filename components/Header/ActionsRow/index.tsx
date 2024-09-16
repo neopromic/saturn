96302547ab67d609688595aa6336b0279817db01
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-09-16 10:54:12 -0300

    feat: add user avatar fallback to header

--- components/Header/ActionsRow/index.tsx
+++ components/Header/ActionsRow/index.tsx
@@ -1,9 +1,16 @@
 import { ModeToggle } from "@/components/toggle-mode";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import Link from "next/link";
 
 export const ActionRow = () => {
-  return (
-    <div className="flex items-center gap-2">
-      <ModeToggle />
-    </div>
-  );
+	return (
+		<div className="flex items-center gap-2">
+			<ModeToggle />
+			<Link href={"/dashboard"}>
+				<Avatar>
+					<AvatarFallback>W</AvatarFallback>
+				</Avatar>
+			</Link>
+		</div>
+	);
 };
