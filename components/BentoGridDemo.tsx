f9614159e372178e8467bb99fa8a6394a15aa4c6
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-09-17 11:54:15 -0300

    feat: renamed the function to BentoGridLayout

--- components/BentoGridDemo.tsx
+++ components/BentoGridDemo.tsx
@@ -12,7 +12,7 @@ import {
 import { motion } from "framer-motion";
 import Image from "next/image";
 
-export function BentoGridDemo() {
+export function BentoGridLayout() {
 	return (
 		<BentoGrid className="max-w-4xl mx-auto md:auto-rows-[20rem]">
 			{items.map((item, i) => (
