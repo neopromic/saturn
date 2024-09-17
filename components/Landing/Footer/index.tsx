ae0386967be4fb785ed481a0cc53414cb5f7882a
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-09-17 12:34:23 -0300

    feat: moved the layout to landing atomized component

---
+++ components/Landing/Footer/index.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button";
+
+export const FooterLayout = () => {
+	return (
+		<section className="mt-12 p-4">
+			<p className="flex flex-col items-center justify-center text-center text-xs text-muted-foreground">
+				© 2024 Saturn Landing Page Builder | Creattek. Todos os direitos
+				reservados.
+			</p>
+			<div className="flex flex-row items-center justify-center">
+				<Button variant={"link"} size={"sm"} className="text-xs">
+					Termos de uso
+				</Button>
+				<Button variant={"link"} size={"sm"} className="text-xs">
+					Política de privacidade
+				</Button>
+				<Button variant={"link"} size={"sm"} className="text-xs">
+					Contatos
+				</Button>
+			</div>
+		</section>
+	);
+};
