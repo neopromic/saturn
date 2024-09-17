9cdf0bdaf2809939b4085006f0fba22fb6e8e09a
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-09-17 12:35:24 -0300

    feat: create landing atomized component

---
+++ components/Landing/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from "./Container";
+import { FeatureSectionLayout } from "./FeatureSection";
+import { FooterLayout } from "./Footer";
+import { HeroLayout } from "./Hero";
+import { HowWeWork } from "./HowWeWork";
+
+export const Landing = {
+	Container,
+	Hero: HeroLayout,
+	Features: FeatureSectionLayout,
+	HowWeWork,
+	Footer: FooterLayout,
+};
