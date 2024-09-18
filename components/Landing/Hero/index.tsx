dd15c8455b1bfd29aa5177b9fcf73939ee217018
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-09-17 22:28:15 -0300

    feat: add br on header

--- components/Landing/Hero/index.tsx
+++ components/Landing/Hero/index.tsx
@@ -23,7 +23,8 @@ export const HeroLayout = () => {
 			</Badge>
 
 			<h1 className="select-none bg-gradient-to-r from-primary to-primary/50 bg-clip-text text-center text-4xl font-extrabold tracking-tighter text-transparent lg:max-w-5xl lg:text-6xl">
-				Crie Landing Pages de Alta Conversão em Minutos
+				Crie Landing Pages de <br className="hidden lg:block" /> Alta Conversão
+				em Minutos
 			</h1>
 			<p className="max-w-80 text-center text-sm text-muted-foreground">
 				Dê vida às suas ideias e aumente suas conversões com facilidade. Nossa
