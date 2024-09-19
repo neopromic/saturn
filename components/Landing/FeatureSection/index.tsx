d39a976cbac605e4e5cc53263857d8cfda22aebc
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-09-18 23:44:01 -0300

    feat: add a gradient to background

--- components/Landing/FeatureSection/index.tsx
+++ components/Landing/FeatureSection/index.tsx
@@ -2,11 +2,11 @@ import { BentoGridLayout } from "@/components/Landing/BentoGridLayout";
 
 export const FeatureSectionLayout = () => {
 	return (
-		<section className="flex min-h-[100vh] flex-col items-center justify-center space-y-2 bg-muted px-4 py-12">
+		<section className="to flex min-h-[100vh] flex-col items-center justify-center space-y-2 bg-gradient-to-b from-muted to-background px-4 py-12">
 			<h2 className="px-4 text-center text-3xl font-bold">
 				Recursos poderosos
 			</h2>
-			<p className="max-w-md text-sm text-center text-muted-foreground">
+			<p className="max-w-md text-center text-sm text-muted-foreground">
 				Nosso editor de arrastar e soltar, combinado com uma biblioteca de
 				modelos incríveis, torna a criação de landing pages tão fácil quanto um
 				jogo de criança.
