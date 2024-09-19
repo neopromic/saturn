512bbaed02ac612ba7b2d0b73c09ab340a512085
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-09-18 23:44:56 -0300

    feat: add a flex to make the feats views beside

--- components/Landing/HowWeWork/index.tsx
+++ components/Landing/HowWeWork/index.tsx
@@ -6,36 +6,38 @@ export const HowWeWork = () => {
 					Como nossa plataforma funciona?
 				</h1>
 			</div>
-			<div className="flex flex-col items-center justify-center space-y-2">
-				<div className="flex size-12 items-center justify-center rounded-full bg-blue-charcoal-500 p-4 text-xl text-blue-charcoal-50">
-					1
+			<div className="items-center gap-2 space-y-4 lg:flex">
+				<div className="flex flex-col items-center justify-center space-y-2">
+					<div className="flex size-12 items-center justify-center rounded-full bg-blue-charcoal-500 p-4 text-xl text-blue-charcoal-50">
+						1
+					</div>
+					<h2 className="text-lg font-medium">
+						Escolha um template {"(Ou use nossa IA)"}.
+					</h2>
+					<p className="max-w-64 text-center text-sm text-muted-foreground">
+						Comece com um de nossos templates profissionais ou um em branco pra
+						criar algo incrível do zero!
+					</p>
 				</div>
-				<h2 className="text-lg font-medium">
-					Escolha um template {"(Ou use nossa IA)"}.
-				</h2>
-				<p className="max-w-64 text-center text-sm text-muted-foreground">
-					Comece com um de nossos templates profissionais ou um em branco pra
-					criar algo incrível do zero!
-				</p>
-			</div>
-			<div className="flex flex-col items-center justify-center space-y-2">
-				<div className="flex size-12 items-center justify-center rounded-full bg-blue-charcoal-500 p-4 text-xl text-blue-charcoal-50">
-					2
+				<div className="flex flex-col items-center justify-center space-y-2">
+					<div className="flex size-12 items-center justify-center rounded-full bg-blue-charcoal-500 p-4 text-xl text-blue-charcoal-50">
+						2
+					</div>
+					<h2 className="text-lg font-medium">Personalize do seu jeito.</h2>
+					<p className="max-w-64 text-center text-sm text-muted-foreground">
+						Use nosso editor arrasta e solta pra adicionar seu conteúdo,
+						imagens, e sua marca.
+					</p>
 				</div>
-				<h2 className="text-lg font-medium">Personalize do seu jeito.</h2>
-				<p className="max-w-64 text-center text-sm text-muted-foreground">
-					Use nosso editor arrasta e solta pra adicionar seu conteúdo, imagens,
-					e sua marca.
-				</p>
-			</div>
-			<div className="flex flex-col items-center justify-center space-y-2">
-				<div className="flex size-12 items-center justify-center rounded-full bg-blue-charcoal-500 p-4 text-xl text-blue-charcoal-50">
-					3
+				<div className="flex flex-col items-center justify-center space-y-2">
+					<div className="flex size-12 items-center justify-center rounded-full bg-blue-charcoal-500 p-4 text-xl text-blue-charcoal-50">
+						3
+					</div>
+					<h2 className="text-lg font-medium">Publique seu site.</h2>
+					<p className="max-w-64 text-center text-sm text-muted-foreground">
+						Com um clique, sua landing page está ativa e pronta pra converter.
+					</p>
 				</div>
-				<h2 className="text-lg font-medium">Publique seu site.</h2>
-				<p className="max-w-64 text-center text-sm text-muted-foreground">
-					Com um clique, sua landing page está ativa e pronta pra converter.
-				</p>
 			</div>
 		</section>
 	);
